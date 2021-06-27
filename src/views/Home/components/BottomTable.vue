0df8d2769287ce9d934eff31060a3564a3b992e7
wangchenchen <1726021@qq.com>
2021-06-27 21:17:28 +0800

    update el table

--- src/views/Home/components/BottomTable.vue
+++ src/views/Home/components/BottomTable.vue
@@ -1,12 +1,46 @@
 <template>
-  <div class="bottom-table-box"></div>
+  <div class="bottom-table-box">
+    <el-table
+      :data="tableData"
+      style="width: 100%"
+      :header-cell-style="{ backgroundColor: '#05052E', color: '#fff', border: none }"
+    >
+      <el-table-column prop="date" label="日期" width="180"> </el-table-column>
+      <el-table-column prop="name" label="姓名" width="180"> </el-table-column>
+      <el-table-column prop="address" label="地址"> </el-table-column>
+    </el-table>
+    <img src="../../../assets/img/home/table-bg.svg" alt="" class="bg-img" />
+  </div>
 </template>
 
 <script>
 export default {
   components: {},
   data() {
-    return {}
+    return {
+      tableData: [
+        {
+          date: '2016-05-02',
+          name: '王小虎',
+          address: '上海市普陀区金沙江路 1518 弄'
+        },
+        {
+          date: '2016-05-04',
+          name: '王小虎',
+          address: '上海市普陀区金沙江路 1517 弄'
+        },
+        {
+          date: '2016-05-01',
+          name: '王小虎',
+          address: '上海市普陀区金沙江路 1519 弄'
+        },
+        {
+          date: '2016-05-03',
+          name: '王小虎',
+          address: '上海市普陀区金沙江路 1516 弄'
+        }
+      ]
+    }
   },
   computed: {},
   watch: {},
@@ -18,8 +52,28 @@ export default {
 <style lang='scss' scoped>
 .bottom-table-box {
   height: 6.53rem;
-  background: url('../../../assets/img/home/table-bg.svg');
-  background-size: cover;
+  padding: 0.2rem;
+  // background: url('../../../assets/img/home/table-bg.svg');
+  // background-size: cover;
   margin-bottom: 50px;
+  position: relative;
+  ::v-deep .el-table__body {
+    border-spacing: 0 8px;
+    border-collapse: separate;
+    table-layout: fixed;
+    background: #05052e;
+    border: none !important;
+  }
+  ::v-deep .el-table tr {
+    background: #080c3d !important;
+    border: none !important;
+  }
+  .bg-img {
+    width: 100%;
+    height: 100%;
+    position: absolute;
+    top: 0;
+    left: 0;
+  }
 }
 </style>
--- src/vonder/index.js
+++ src/vonder/index.js
@@ -10,11 +10,15 @@ import {
     Row,
     Col,
     Pagination,
-    Select
+    Select,
+    Table,
+    TableColumn
 } from 'element-ui'
 import lang from 'element-ui/lib/locale/lang/en'
 import locale from 'element-ui/lib/locale'
 locale.use(lang)
-const ary = [Menu, Submenu, MenuItem, MenuItemGroup, Scrollbar, Input, Row, Col, Pagination, Select]
+const ary = [Menu, Submenu, MenuItem, MenuItemGroup, Scrollbar, Input, Row, Col, Pagination, Select, Table,
+    TableColumn
+]
 ary.forEach(component => Vue.use(component))
 Vue.prototype.$message = Message
