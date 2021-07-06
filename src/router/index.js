f9a2b196d69a47f8be383237c0f28dd26ed24b15
szfdiwang <wangchenchen@matrixelements.com>
2021-07-06 10:48:06 +0800

    update api

--- src/router/index.js
+++ src/router/index.js
@@ -7,75 +7,73 @@ import Layout from '@/layout'
 Vue.use(VueRouter)
 
 const routes = [{
-    path: '/',
-    component: Layout,
-    redirect: '/home',
-    children: [{
-      path: '/home',
-      name: 'home',
-      component: Home
-    }]
+  path: '/',
+  component: Layout,
+  redirect: '/home',
+  children: [{
+    path: '/home',
+    name: 'home',
+    component: Home
+  }]
+},
+{
+  path: '/node',
+  component: Layout,
+  children: [{
+    path: '/node/index',
+    name: 'node',
+    component: () => import(/* webpackChunkName: "node" */ '../views/Node/Node.vue')
   },
   {
-    path: '/node',
-    component: Layout,
-    children: [{
-        path: '/node/index',
-        name: 'node',
-        component: () => import( /* webpackChunkName: "node" */ '../views/Node/Node.vue')
-      },
-      {
-        path: '/node/NodeDetail',
-        name: 'nodeDetail',
-        component: () => import( /* webpackChunkName: "NodeDetail" */ '../views/Node/NodeDetail.vue')
-      },
-      {
-        path: '/node/MetaData',
-        name: 'MetaData',
-        component: () => import( /* webpackChunkName: "MetaData" */ '../views/Node/MetaData.vue')
-      }
-    ]
+    path: '/node/NodeDetail',
+    name: 'nodeDetail',
+    component: () => import(/* webpackChunkName: "NodeDetail" */ '../views/Node/NodeDetail.vue')
   },
   {
-    path: '/data',
-    component: Layout,
-    children: [{
-        path: '/data/index',
-        name: 'data',
-        component: () => import( /* webpackChunkName: "data" */ '../views/Data/Data.vue')
-      },
-      {
-        path: '/data/DataDetail',
-        name: 'dataDetail',
-        component: () => import( /* webpackChunkName: "DataDetail" */ '../views/Data/DataDetail.vue')
-      }
-    ]
-  },
-  {
-    path: '/task',
-    component: Layout,
-    children: [{
-        path: '/task/index',
-        name: 'task',
-        component: () => import( /* webpackChunkName: "TaskHome" */ '../views/Task/Task.vue')
-      },
-
-      {
-        path: '/task/TaskDetail',
-        name: 'taskDetail',
-        component: () => import( /* webpackChunkName: "TaskDetail" */ '../views/Task/TaskDetail.vue')
-      },
-      {
-        path: '/task/TaskLog',
-        name: 'TaskLog',
-        component: () => import( /* webpackChunkName: "TaskLog" */ '../views/Task/TaskLog.vue')
-      }
-    ]
-
+    path: '/node/MetaData',
+    name: 'MetaData',
+    component: () => import(/* webpackChunkName: "MetaData" */ '../views/Node/MetaData.vue')
   }
+  ]
+},
+{
+  path: '/data',
+  component: Layout,
+  children: [{
+    path: '/data/index',
+    name: 'data',
+    component: () => import(/* webpackChunkName: "data" */ '../views/Data/Data.vue')
+  },
+  {
+    path: '/data/DataDetail',
+    name: 'dataDetail',
+    component: () => import(/* webpackChunkName: "DataDetail" */ '../views/Data/DataDetail.vue')
+  }
+  ]
+},
+{
+  path: '/task',
+  component: Layout,
+  children: [{
+    path: '/task/index',
+    name: 'task',
+    component: () => import(/* webpackChunkName: "TaskHome" */ '../views/Task/Task.vue')
+  },
+
+  {
+    path: '/task/TaskDetail',
+    name: 'taskDetail',
+    component: () => import(/* webpackChunkName: "TaskDetail" */ '../views/Task/TaskDetail.vue')
+  },
+  {
+    path: '/task/TaskLog',
+    name: 'TaskLog',
+    component: () => import(/* webpackChunkName: "TaskLog" */ '../views/Task/TaskLog.vue')
+  }
+  ]
+
+}
 ]
-
-
 
 const router = new VueRouter({
   mode: 'history',
